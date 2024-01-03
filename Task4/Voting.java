725a8ce06bd837143b90b2922d0e01128470e10b
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-01-03 22:35:48 +0530

    Add files via upload

---
+++ Task4/Voting.java
@@ -0,0 +1,12 @@
+public class voting{
+ public static void main(String[] args){
+     int votingage=18;
+     int Currentage=21;
+     if(Currentage>=votingage){
+     System.out.print("Eligible"); 
+     }
+     else{
+     System.out.print("Not eligible");    
+     }
+   }
+}
