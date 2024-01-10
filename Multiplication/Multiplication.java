9cf8eeff25e73890f7b0ba04b5d794e7e2883ed7
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-01-10 21:56:24 +0530

    Add files via upload

---
+++ Multiplication/Multiplication.java
@@ -0,0 +1,9 @@
+public class Multiplication{
+  public static void main(String [] args){
+      int num = 10;
+      for( int i = 0; i<=num;i++){
+     System.out.println(i + "*" + num + "=" + i * num);
+     }
+
+    }
+}
