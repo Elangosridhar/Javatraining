944d7bde006e576f874a5ebbd5f033f7e6d3445e
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-01-10 21:55:40 +0530

    Add files via upload

---
+++ inputs/Input.java
@@ -0,0 +1,8 @@
+public class Input{
+   public static void main(String  args[]){
+   System.out.println("no.of.Arguments: " + args.length);
+    for(int i=0;i<=args.length;i++){
+     System.out.println(args[i]);
+     }
+   }
+}
