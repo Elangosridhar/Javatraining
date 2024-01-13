c10ff3d7335683b62886dced8d50e2ab9e193f25
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-01-13 16:13:38 +0530

    Add files via upload

---
+++ odd numbers/Oddnum.java
@@ -0,0 +1,10 @@
+public class Oddnum{
+  public static void main(String args[]){
+      int n =100;
+      for(int i= 50;i<=n;i++){
+    if(i%2==1){
+       System.out.println("Sum of odd is: " +i); 
+       }
+        }
+      }
+   }
