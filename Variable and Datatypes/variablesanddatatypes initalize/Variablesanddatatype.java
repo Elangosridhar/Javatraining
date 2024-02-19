745cc5beec598d727e15276b5ae7da9f4d021a67
Elangosridhar <152356157+Elangosridhar@users.noreply.github.com>
2024-02-19 15:33:43 +0530

    Add files via upload

---
+++ Variable and Datatypes/variablesanddatatypes initalize/Variablesanddatatype.java
@@ -0,0 +1,40 @@
+public class Variablesanddatatype{
+    public static void main(String[] args){
+//Declare a variable of type int named age and assign it the value 25.
+     int age = 25;
+ System.out.println(age);
+ //Create a variable of type double named temperature and assign it the value 98.6
+   double temperature = 98.8;
+System.out.println(temperature);
+//Declare a variable of type boolean named isRaining and assign it the value true.
+  boolean isRaining = true;
+System.out.println(isRaining);
+//Create a variable of type char named grade and assign it the value 'A'  
+   char grade = 'A';
+System.out.println(grade);
+//Declare a variable of type String named name and assign it the value "John Doe".
+  String name = "John Doe";
+System.out.println(name);
+//Initialize three variables num1, num2, and num3 of type int with values 10, 20, and 30 respectively.
+   int num1 = 10;
+   int num2 = 20;
+   int num3 = 30;
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+//Create a variable pi of type double and assign it the value of pi (use the Math.PI constant).
+double pi = Math.PI;
+System.out.println(pi);
+//Declare a variable isEven of type boolean and assign it the result of checking whether num1 is an even number.
+boolean isEven = num1 % 2 == 0;
+System.out.println(isEven);
+//Initialize a variable initial of type char with the initial letter of your first name
+char initial = 'S';
+System.out.println(initial);
+//Declare a variable city of type String and assign it the name of your favorite city.
+String city = "Vellore";
+System.out.println(city);
+//
+
+   }
+}
